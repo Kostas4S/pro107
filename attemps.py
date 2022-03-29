118e11fac86421781bd2ac477fb45d750756a6ad
Kostas4S <81780796+Kostas4S@users.noreply.github.com>
2022-03-29 19:56:19 -0400

    Update attemps.py

--- attemps.py
+++ attemps.py
@@ -6,6 +6,6 @@ df = pd.read_csv("data.csv")
 mean = df.groupby(["student_id","level"], as_index = False)["attempt"].mean()
 
 fig = px.scatter(mean,x = "student_id",y ="level",
-    orientation = 'h'
+    size="attempt", color="attempt"
 )
 fig.show()
